db30ddb46df78d7383417bed793d9fa457c8115e
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-10-20 21:23:47 +0530

    changes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { userController, verifyToken } from "../controllers/userController.js";
-import { getPdfHistory, pdfExtract, uploadPdf } from "../controllers/pdfuploader.js";
+import { getPdfHistory, pdfExtract, uploadPdf } from "../controllers/pdfController.js";
 import uploader from "../middlewares/uploader.js";
 import { Auth } from "../middlewares/auth.js";
 
