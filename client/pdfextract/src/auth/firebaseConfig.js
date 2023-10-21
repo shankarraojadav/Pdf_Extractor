30f881e9f718530719826c1341b5a23c68b3e814
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-10-21 09:11:02 +0530

    changes

--- client/pdfextract/src/auth/firebaseConfig.js
+++ client/pdfextract/src/auth/firebaseConfig.js
@@ -2,14 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env
+  apiKey: process.env.VITE_REACT_APP_FIREBASE_API_KEY || import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+
+  authDomain: process.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN || import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+
+  projectId: process.env.VITE_REACT_APP_FIREBASE_PROJECT_ID || import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+
+  storageBucket: process.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET || import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+
+  messagingSenderId: process.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID || import.meta.env
     .VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID,
+
+  appId: process.env.VITE_REACT_APP_FIREBASE_APP_ID || import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+
+  measurementId: process.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID || import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
